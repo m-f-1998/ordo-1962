d7109ce3d3bdb3cd7eb78044d0245f5cf5b1ce6c
Matthew Frankland <admin@matthewfrankland.co.uk>
2024-03-14 22:46:10 +0000

    updated support page

---
+++ api/support/js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener ( "load", ( ) => {
+  if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-bs-theme','dark')
+  } else {
+    document.documentElement.setAttribute('data-bs-theme','light')
+  }
+}, { passive: true })
